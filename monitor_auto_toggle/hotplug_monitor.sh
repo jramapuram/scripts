6531ad9a3e4f55672d6ba764e19de86c8906359c
Jason Ramapuram <jason.ramapuram@gmail.com>
2017-09-29 09:54:46 +0200

    add displaylink support

--- monitor_auto_toggle/hotplug_monitor.sh
+++ monitor_auto_toggle/hotplug_monitor.sh
@@ -9,35 +9,43 @@ export XAUTHORITY=/home/$X_USER/.Xauthority
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
 INTERNAL_OUTPUT="eDP1"
+EXTERNAL_OUTPUT="DVI-I-1-1"
+EXTERNAL_POSTFIX="DVI-I-1"
+# EXTERNAL_POSTFIX="DP-1" # thunderbolt
+# EXTERNAL_OUTPUT="DP1" # thunderbolt
 
 # determine what index the card is assigned
-if [ -d "/sys/class/drm/card0" ]; then
-    CARD_INDEX="/sys/class/drm/card0"
-else
+if [ -d "/sys/class/drm/card1" ]; then
     CARD_INDEX="/sys/class/drm/card1"
+else
+    CARD_INDEX="/sys/class/drm/card0"
 fi
     
-if [ $(cat ${CARD_INDEX}-DP-1/status) == "connected" ] ; then
+if [ $(cat ${CARD_INDEX}-$EXTERNAL_POSTFIX/status) == "connected" ] ; then
     # If at home disable laptop screen else use 'all' mode
-    if [ $(head -n 1 ${CARD_INDEX}-DP-1/modes) == "3440x1440" ] ; then
-	xrandr --output $INTERNAL_OUTPUT --off --output DP1 --auto
+    if [ $(head -n 1 ${CARD_INDEX}-$EXTERNAL_POSTFIX/modes) == "3440x1440" ] ; then
+	xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
     else
-	xrandr --output $INTERNAL_OUTPUT --auto --output DP1 --auto --left-of $INTERNAL_OUTPUT
+	xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --left-of $INTERNAL_OUTPUT
     fi
     killall -SIGUSR1 conky
     sleep 1 && nitrogen --restore
-elif [ $(cat ${CARD_INDEX}-DP-1/status) == "disconnected" ] ; then
-    xrandr --output $INTERNAL_OUTPUT --auto --output DP1 --off
+    i3-msg 'restart'    
+elif [ $(cat ${CARD_INDEX}-$EXTERNAL_POSTFIX/status) == "disconnected" ] ; then
+    xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
     killall -SIGUSR1 conky
     sleep 1 && nitrogen --restore
+    i3-msg 'restart'
 elif [ $(cat ${CARD_INDEX}-HDMI-A-1/status) == "connected" ] ; then
     xrandr --output $INTERNAL_OUTPUT --auto --output HDMI1 --auto --left-of $INTERNAL_OUTPUT
     killall -SIGUSR1 conky
     sleep 1 && nitrogen --restore
+    i3-msg 'restart'
 elif [ $(cat ${CARD_INDEX}-HDMI-A-1/status) == "disconnected" ] ; then
     xrandr --output $INTERNAL_OUTPUT --auto --output HDMI1 --off
     killall -SIGUSR1 conky
     sleep 1 && nitrogen --restore
+    i3-msg 'restart'    
 else
     exit
 fi
